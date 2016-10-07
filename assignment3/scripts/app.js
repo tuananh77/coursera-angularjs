002fc4e8ba08ece9d9c3cfd39a68a6b45dc358f0
Tuan Anh <tuananh77@gmail.com>
2016-10-07 15:06:00 -0400

    final version

---
+++ assignment3/scripts/app.js
@@ -0,0 +1,87 @@
+(function () {
+  'use strict';
+
+  angular.module("NarrowItDownApp", [])
+  .controller("NarrowItDownController", NarrowItDownController)
+  .service("MenuSearchService", MenuSearchService)
+  .directive("foundItems", FoundItemsDirective)
+  .constant("ApiBasePath", "https://davids-restaurant.herokuapp.com");
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'itemList.html',
+      scope: {
+        found: '<',
+        showMsg: '<',
+//        myTitle: '@title',
+        onRemove: '&'
+      },
+      controller: FoundItemsDirectiveController,
+      controllerAs: 'list',
+      bindToController: true
+    };
+
+    return ddo;
+  }
+
+  function FoundItemsDirectiveController() {
+    var dir = this;
+  }
+
+  NarrowItDownController.$inject = ["MenuSearchService"];
+  function NarrowItDownController(MenuSearchService) {
+    var ctrl = this;
+
+    ctrl.found = [];
+    ctrl.showMsg = false;
+
+    ctrl.getMatchedMenuItems = function(searchTerm) {
+      var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+      // console.log(promise);
+      return promise.then(function(result) {
+        // console.log(result);
+        ctrl.found = result;
+        ctrl.showMsg = true;
+        return result;
+      });
+    }
+
+    ctrl.removeItem = function (index) {
+      ctrl.found.splice(index, 1);
+      ctrl.showMsg = false;
+    }
+  }
+
+  MenuSearchService.$inject = ['$http', 'ApiBasePath']
+  function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+
+    this.getMatchedMenuItems = function (searchTerm) {
+      return $http({
+        method: 'GET',
+        url: ApiBasePath + "/menu_items.json"
+      })
+      .then(function (result) { //if success
+        // process result and only keep items that match
+        var items = result.data.menu_items;
+        var foundItems = [];
+        // console.log(result.data.menu_items);
+        for (var i in items) {
+          if (items[i].description.indexOf(searchTerm) !== -1)
+            foundItems.push(items[i]);
+        }
+        // return processed items
+        // console.log(foundItems);
+        return foundItems;
+      });
+      // .catch(function (result) { //if fail
+      //     console.log("Failed to retrieve menu items.");
+      //     return null;
+      // });
+
+      // return foundItems;
+    };
+  }
+
+})();
